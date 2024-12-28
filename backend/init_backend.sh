8cc746270199a4a7003a5056ceda8c2bf9f1e378
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2024-12-28 07:09:23 +0000

    test6

--- backend/init_backend.sh
+++ backend/init_backend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+# Wait for the database to be ready
+echo "Waiting for the database to be ready..."
+until nc -z -v -w30 db 5432
+do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 1
+done
+echo "PostgreSQL is up and running!"
+
 # Run database migrations
 flask db init || echo "Database already initialized."
 flask db migrate -m "initial migration"
